6c537fd6eec8661077cfd9283664859481eee2f1
grahk93 <grace.juster@flatironschool.com>
2016-06-13 12:47:43 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+  attr_accessor :word
+  #able to call .word, .word=()
+
+  #initialize every new instance of anagram class with a word value
+  def initialize(word)
+    @word = word
+  end
+
+  #find the anagrams for a word in a list of possible matches
+  #convert the possible matches to an array %w
+  #use collect to itterate and return an array
+  #split and sort each possible word and given word (self)
+  #if equal add return
+  def match (pos_matches)
+    #binding.pry
+    pos_matches.select do |pos|
+      pos.split("").sort == self.word.split("").sort
+    end
+  end
+
+end
+#Pry.start
